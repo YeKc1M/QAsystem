f4a89f90d53a6864023da8cfe041ef2330e2dfc4
YeKc1M <15008181891@163.com>
2020-04-10 20:55:52 +0800

    add ajax post on question_button

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask.helpers import url_for
 
 from Modelling import calculateAllNames, calculateLogical
@@ -10,8 +10,19 @@ def testModelling():
     sims=calculateAllNames('explore knowledge sources step 1')
     print(sorted(enumerate(sims), key=lambda x: x[1], reverse=True)[0])
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
+    if request.method=='POST':
+        question=request.form['question']
+        print(question)
+        sims=calculateAllNames(question)
+        maxSim=sorted(enumerate(sims), key=lambda x:x[1], reverse=True)[0]
+        print(str(maxSim[0])+' '+str(maxSim[1]))
+        if(maxSim[1]>0.4):
+            print('calculate logical')
+        else:
+            print('return 3')
+        return jsonify(result=question+' back')
     return render_template('index.html')
 
 if __name__=='__main__':
